142a94aec792a6476eeed8701ddb2bfc2f8f45a9
Bartosz Nosowski <barteknosowski@gmail.com>
2020-09-07 11:40:59 +0200

    initial commit

---
+++ src/main/java/pl/akademiakodu/AK_Spring_ExerciseMethod_BasicCalculatorTODO/models/CalculatorAverage.java
@@ -0,0 +1,49 @@
+package pl.akademiakodu.AK_Spring_ExerciseMethod_BasicCalculatorTODO.models;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorAverage {
+    /**
+     * Method calculate mathematical square of digit
+     * @param a user input
+     * @return square of a
+     */
+    public int squareOf(int a){
+        return a * a;
+    }
+    /**
+     * Method calculate mathematical element of digit
+     * @param a user input
+     * @return element of a
+     */
+    public int elementOf(int a){
+        double result = Math.sqrt(a);
+        return (int) result;
+    }
+    /**
+     * Method calculate mathematical factorial of digit
+     * @param a user input
+     * @return factorial of a
+     */
+    public static int factorial(int a){
+        int result = 1;
+        for (int i = 1; i <= a; i++) {
+            result *= i;
+        }
+        return result;
+    }
+    /**
+     * Method check if digit is a prime number
+     * @param a user input
+     * @return result
+     */
+    public boolean isPrimeNumber(int a){
+        for (int i = 2; i < a ; i++) {
+            if (a%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
